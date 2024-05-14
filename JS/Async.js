080d08bd3f2ef3ae552de4a1831246a3b7d309e3
huongnobi200 <huongnobi2000@gmail.com>
2024-05-14 12:54:22 +0700

    js promises demo

---
+++ JS/Async.js
@@ -0,0 +1,30 @@
+// setTimeout(nameFunction, time)
+//Promises
+// Status of a JS Promise:
+//     + pending: chờ
+//     + Fulfilled: hoàn thành
+//     + Rejected: từ chối
+// Promise object supports two properties: state ang result
+    // + pending - undefined
+    // + fulfilled - value
+    // + rejected - an error object
+
+
+
+// let myPromises = new Promise((myResolve, myReject) => {
+//     let x = 0;
+
+//     if (x === 0) myResolve("x == 0")
+//     else myReject("x khac 0");
+// });
+
+// myPromises.then(
+//     function(value) {
+//         console.log('Success ', value);
+//     },
+//     function(err) {
+//         console.log('Error', err);
+//     }
+// );
+
+
